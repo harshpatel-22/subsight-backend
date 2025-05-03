42339159975c3c34ed4e3f789d1adbd65f18dbd3
Harsh Patel <harshvpatel1508@gmail.com>
2025-05-03 15:05:09 +0530

    access origin from all

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000
 // ---------- Middleware ----------
 app.use(
 	cors({
-		origin: ['http://localhost:3000', 'https://subsight.vercel.app/'],
+		origin: '*',
 		credentials: true,
 	})
 )
