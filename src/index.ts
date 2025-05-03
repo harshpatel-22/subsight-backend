4f778a63885ebbcaf02f3f41dc51d4748f6d05b1
Harsh Patel <harshvpatel1508@gmail.com>
2025-05-03 15:15:40 +0530

    added two cors url

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000
 // ---------- Middleware ----------
 app.use(
 	cors({
-		origin: '*',
+		origin: ['http://localhost:3000', 'https://subsight.vercel.app/'],
 		credentials: true,
 	})
 )
